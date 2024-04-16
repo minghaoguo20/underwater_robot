c1a34e344ec1b6cf370d23b07804002b8b45b3c0
Henry <ihenrykwok@outlook.com>
2024-04-16 16:53:10 +0800

    update

--- utils/line.py
+++ utils/line.py
@@ -7,7 +7,7 @@ from sklearn.cluster import KMeans
 
 def get_lines(frame, lower_hsv, upper_hsv):
     """
-    获取直线
+    get lines from frame
     """
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask1 = cv2.inRange(hsv, lowerb=lower_hsv, upperb=upper_hsv)
@@ -15,6 +15,7 @@ def get_lines(frame, lower_hsv, upper_hsv):
     mask1 = cv2.dilate(mask1, kernel1)
     mask1 = cv2.erode(mask1, kernel1)
     lines = cv2.HoughLinesP(mask1, 1, np.pi/180, 100, minLineLength=100, maxLineGap=50)
+    lines = np.squeeze(lines, axis=1)
     return lines
 
 def calculate_line_center_and_angle(x0, y0, x1, y1):
@@ -26,6 +27,21 @@ def calculate_line_center_and_angle(x0, y0, x1, y1):
     
     return (center_x, center_y), angle
 
+def get_center_and_angle(lines):
+    """
+    input: numpy array (index, coordinates)
+        coordinates: x0, y0, x1, y1
+    output: numpy array (index, data)
+        data: center_x, center_y, angle
+    """
+    center_x = (lines[:, 0] + lines[:, 2]) / 2
+    center_y = (lines[:, 1] + lines[:, 3]) / 2
+
+    angle = np.arctan2(lines[:, 3] - lines[:, 1], lines[:, 2] - lines[:, 0]) * 180 / np.pi
+
+    return np.array([center_x, center_y, angle]).T
+
+
 def cluster(data, n_clusters = 2):
     """
     input: numpy array
@@ -34,7 +50,7 @@ def cluster(data, n_clusters = 2):
     kmeans.fit(data)
     return kmeans.labels_
 
-def process_lines(lines):
+def process_lines(lines, center_and_angle):
     if lines is None:
         return False
     
